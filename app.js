c0f4b2cf2729318c7ab3b1c26685878e9dc664fc
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-04-15 20:56:04 +0200

    log  post entries in home page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 
 // eslint-disable-next-line no-unused-vars
 const ejs = require('ejs')
+const ejsLint = require('ejs-lint')
 
 const homeStartingContent =
     'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.'
@@ -24,7 +25,7 @@ let posts = []
 
 //home route
 app.get('/', (req, res) => {
-    res.render('home', { homeContent: homeStartingContent })
+    res.render('home', { homeContent: homeStartingContent, posts: posts })
     console.log(posts)
 })
 
