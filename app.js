ff858bd33394087e5a9175ab73f7d78e12074dc7
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-04-15 22:32:17 +0200

    use Lodash utility to match post title with its url

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express')
 // eslint-disable-next-line no-unused-vars
 const ejs = require('ejs')
 // const ejsLint = require('ejs-lint')
+const _ = require('lodash')
 
 const homeStartingContent =
     'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.'
@@ -55,7 +56,13 @@ app.post('/compose', (req, res) => {
 })
 
 app.get('/posts/:postName', (req, res) => {
-    console.log(req.params.postName)
+    const requeestedTitle = _.lowerCase(req.params.postName)
+    posts.forEach((post) => {
+        const storedTitle = _.lowerCase(post.title)
+        if (storedTitle === requeestedTitle) {
+            console.log('match found')
+        }
+    })
 })
 
 app.listen(2000, () => {
